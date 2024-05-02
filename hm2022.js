55c5780d7ee864d41eea16b201f92adf9ab27fb4
unknown <h2023sep15@outlook.com>
2024-05-02 15:32:24 -0500

    Added js file

--- hm2022.js
+++ hm2022.js
@@ -1,20 +1,57 @@
-function(exitButton){
-    document.getElementById("exit");
-}
-function init () {
-    const missionAbort = document.getElementById("leave a comment");
-    const button = document.getElementById("exit");
-    const paragraph = document.getElementById("statusReport");
-    //button.addEventListener("click",function(exitButton))
-    let div = document.createElement('div');
-    div.classList.add('exitmenu')
 
-
+// 3:04 PM 5/2/2024
+    
+function add(){
+    var a = parseInt(document.getElementById("First").value);
+    var b = parseInt(document.getElementById("Second").value);
+    var c = a + b;
+    document.getElementById("answer").value = c;
 }
 
-window.addEventListener("load", init);
+function exit(){
+    var f = parseInt(document.getElementById("First name").value);
+    var d = "Goodbye !";
+    var bye = console.log(d+f);
+    document.getElementById("exit protocol").value = d;
+}
 
 
+
+// 2:42 PM 5/2/2024
+// console.log("Hello welcome to my website !");
+// // When you rtClk and inspect and press console
+// // we can see that the console displays"Hello welcome to my website !" 
+// var name = "Hannah website";//string
+// var a = 20;
+// a = "Hannah";
+// console.log(a);
+// var GroceryItems = ["Milk","Oranges"];
+// // console.log(GroceryItems);
+// //Built-in functions 3:04 PM 5/2/2024
+// GroceryItems = GroceryItems.concat(23);
+// console.log(GroceryItems);
+
+
+
+
+
+// function(exitButton){
+//     document.getElementById("exit");
+// }
+// function init () {
+//     const missionAbort = document.getElementById("leave a comment");
+//     const button = document.getElementById("exit");
+//     const paragraph = document.getElementById("statusReport");
+//     //button.addEventListener("click",function(exitButton))
+//     let div = document.createElement('div');
+//     div.classList.add('exitmenu')
+
+
+// }
+
+// window.addEventListener("load", init);
+
+// .js file basically adds soe javvascript code for the dynamic functionality
 // function init () {
 //     const missionAbort = document.getElementById("leave a comment");
 //     const button = document.getElementById("Select a photo album");
